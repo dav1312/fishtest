d60ba284027640fe66969386d02021239c29b49f
dav <63931154+dav1312@users.noreply.github.com>
2021-12-22 22:20:44 +0100

    Prettier indentation 4 spaces 200 print width

--- server/fishtest/static/js/application.js
+++ server/fishtest/static/js/application.js
@@ -1,110 +1,111 @@
 // https://stackoverflow.com/questions/14267781/sorting-html-table-with-javascript
 // https://stackoverflow.com/questions/40201533/sort-version-dotted-number-strings-in-javascript
 const getCellValue = (tr, idx) => tr.children[idx].dataset.diff || tr.children[idx].innerText || tr.children[idx].textContent;
-const padDotVersion = (dn) => dn.split('.').map(n => +n+1000).join('');
+const padDotVersion = (dn) =>
+    dn
+        .split(".")
+        .map((n) => +n + 1000)
+        .join("");
 let p1, p2;
-const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
-    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2)
-    ? v1 - v2
-    : v1 !== '' && v2 !== '' && !isNaN(p1 = padDotVersion(v1)) && !isNaN(p2 = padDotVersion(v2))
-    ? p1 - p2
-    : v1.toString().localeCompare(v2)
-)(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+const comparer = (idx, asc) => (a, b) =>
+    ((v1, v2) =>
+        v1 !== "" && v2 !== "" && !isNaN(v1) && !isNaN(v2)
+            ? v1 - v2
+            : v1 !== "" && v2 !== "" && !isNaN((p1 = padDotVersion(v1))) && !isNaN((p2 = padDotVersion(v2)))
+            ? p1 - p2
+            : v1.toString().localeCompare(v2))(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
 
 $(() => {
     // clicking on table headers sorts the rows
-    $(document).on('click', 'th', (event) => {
+    $(document).on("click", "th", (event) => {
         const th = $(event.currentTarget)[0];
-        const table = th.closest('table');
-        const body = table.querySelector('tbody');
-        Array.from(body.querySelectorAll('tr'))
-            .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
-            .forEach(tr => body.appendChild(tr));
+        const table = th.closest("table");
+        const body = table.querySelector("tbody");
+        Array.from(body.querySelectorAll("tr"))
+            .sort(comparer(Array.from(th.parentNode.children).indexOf(th), (this.asc = !this.asc)))
+            .forEach((tr) => body.appendChild(tr));
     });
 
     // clicking Show/Hide on Pending + Paused tests and Machines sections toggles them
-    $("#pending-button").click(function() {
-        var active = $(this).text().trim() === 'Hide';
-        $(this).text(active ? 'Show' : 'Hide');
+    $("#pending-button").click(function () {
+        var active = $(this).text().trim() === "Hide";
+        $(this).text(active ? "Show" : "Hide");
         $("#pending").slideToggle(150);
-        $.cookie('pending_state', $(this).text().trim());
+        $.cookie("pending_state", $(this).text().trim());
     });
 
-    $("#paused-button").click(function() {
-        var active = $(this).text().trim() === 'Hide';
-        $(this).text(active ? 'Show' : 'Hide');
+    $("#paused-button").click(function () {
+        var active = $(this).text().trim() === "Hide";
+        $(this).text(active ? "Show" : "Hide");
         $("#paused").slideToggle(150);
-        $.cookie('paused_state', $(this).text().trim());
+        $.cookie("paused_state", $(this).text().trim());
     });
 
-    $("#non_default-button").click(function() {
-        var active = $(this).text().trim().substring(0, 4) === 'Hide';
-        $(this).text(active ? 'Show non default nets' : 'Hide non default nets');
-        $.cookie('non_default_state', active ? 'Hide' : 'Show');
+    $("#non_default-button").click(function () {
+        var active = $(this).text().trim().substring(0, 4) === "Hide";
+        $(this).text(active ? "Show non default nets" : "Hide non default nets");
+        $.cookie("non_default_state", active ? "Hide" : "Show");
         window.location.reload();
     });
 
     let fetchingMachines = false;
-    $("#machines-button").click(function() {
-        const active = $(this).text().trim() === 'Hide';
-        $(this).text(active ? 'Show' : 'Hide');
+    $("#machines-button").click(function () {
+        const active = $(this).text().trim() === "Hide";
+        $(this).text(active ? "Show" : "Hide");
         if (!active && !$("#machines table")[0] && !fetchingMachines) {
             fetchingMachines = true;
             $.get("/tests/machines", (html) => $("#machines").append(html));
         }
         $("#machines").toggle();
-        $.cookie('machines_state', $(this).text().trim());
+        $.cookie("machines_state", $(this).text().trim());
     });
 
     // Click the sun/moon icons to change the color theme of the site
     // SRI hash for "fishtest/server/fishtest/static/css/theme.dark.css":
     // openssl dgst -sha256 -binary theme.dark.css | openssl base64 -A
-    let theme = $.cookie('theme') || 'light';
-    $("#change-color-theme").click(function() {
-      if (theme === 'light') {
-        const darkThemeSha256 = $("meta[name='dark-theme-sha256']").attr("content");
-        $("#sun").show();
-        $("#moon").hide();
-        $("<link>")
-          .attr("href", "/css/theme.dark.css?v=" + darkThemeSha256)
-          .attr("rel", "stylesheet")
-          .attr("integrity", "sha256-" + darkThemeSha256)
-          .appendTo($("head"));
-        theme = 'dark';
-      } else {
-        $("#sun").hide();
-        $("#moon").show();
-        $('head link[href*="/css/theme.dark.css"]').remove();
-        theme = 'light';
-      }
-      let cookieExpireDate = new Date();
-      // Remember the theme for 30 days
-      cookieExpireDate.setTime(cookieExpireDate.getTime() + 30 * 24 * 60 * 60 * 1000);
-      $.cookie('theme', theme, {
-        path: '/',
-        expires: cookieExpireDate,
-      });
+    let theme = $.cookie("theme") || "light";
+    $("#change-color-theme").click(function () {
+        if (theme === "light") {
+            const darkThemeSha256 = $("meta[name='dark-theme-sha256']").attr("content");
+            $("#sun").show();
+            $("#moon").hide();
+            $("<link>")
+                .attr("href", "/css/theme.dark.css?v=" + darkThemeSha256)
+                .attr("rel", "stylesheet")
+                .attr("integrity", "sha256-" + darkThemeSha256)
+                .appendTo($("head"));
+            theme = "dark";
+        } else {
+            $("#sun").hide();
+            $("#moon").show();
+            $('head link[href*="/css/theme.dark.css"]').remove();
+            theme = "light";
+        }
+        let cookieExpireDate = new Date();
+        // Remember the theme for 30 days
+        cookieExpireDate.setTime(cookieExpireDate.getTime() + 30 * 24 * 60 * 60 * 1000);
+        $.cookie("theme", theme, {
+            path: "/",
+            expires: cookieExpireDate,
+        });
     });
 
     // CSRF protection for links and forms
-    const csrfToken = $("meta[name='csrf-token']").attr('content');
+    const csrfToken = $("meta[name='csrf-token']").attr("content");
     $("#logout").on("click", (event) => {
         event.preventDefault();
         $.ajax({
             url: "/logout",
             method: "POST",
             headers: {
-                'X-CSRF-Token': csrfToken
+                "X-CSRF-Token": csrfToken,
             },
             success: () => {
                 window.location = "/";
-            }
+            },
         });
     });
     $("form[method='POST']").each((i, $form) => {
-        $("<input>")
-            .attr("type", "hidden")
-            .attr("name", "csrf_token").val(csrfToken)
-            .appendTo($form);
+        $("<input>").attr("type", "hidden").attr("name", "csrf_token").val(csrfToken).appendTo($form);
     });
 });
--- server/fishtest/static/js/sprt.js
+++ server/fishtest/static/js/sprt.js
@@ -5,61 +5,61 @@
   See http://hardy.uhasselt.be/Fishtest/sprta.pdf for more information.
 */
 
-const nelo_divided_by_nt=800/Math.log(10);
+const nelo_divided_by_nt = 800 / Math.log(10);
 
 function L(x) {
-    return 1/(1+Math.pow(10,-x/400));
+    return 1 / (1 + Math.pow(10, -x / 400));
 }
 
 function Linv(x) {
-    return -400*Math.log10(1/x-1);
+    return -400 * Math.log10(1 / x - 1);
 }
 
-function PT(LA,LB,h){
+function PT(LA, LB, h) {
     // Universal functions
-    var P,T;
-    if(Math.abs(h*(LA-LB))<1e-6){
-	// avoid division by zero
-        P=-LA/(LB-LA);
-        T=-LA*LB;
-    }else{
-        var exp_a=Math.exp(-h*LA);
-        var exp_b=Math.exp(-h*LB);
-        P=(1-exp_a)/(exp_b-exp_a);
-        T=(2/h)*(LB*P+LA*(1-P));
+    var P, T;
+    if (Math.abs(h * (LA - LB)) < 1e-6) {
+        // avoid division by zero
+        P = -LA / (LB - LA);
+        T = -LA * LB;
+    } else {
+        var exp_a = Math.exp(-h * LA);
+        var exp_b = Math.exp(-h * LB);
+        P = (1 - exp_a) / (exp_b - exp_a);
+        T = (2 / h) * (LB * P + LA * (1 - P));
     }
-    return [P,T];
+    return [P, T];
 }
 
-function Sprt(alpha,beta,elo0,elo1,draw_ratio,rms_bias,elo_model) {
-    var rms_bias_score=L(rms_bias)-0.5;
-    var variance3=(1-draw_ratio)/4.0;
-    this.variance=variance3-Math.pow(rms_bias_score,2);
-    if(this.variance<=0){
-	return;
+function Sprt(alpha, beta, elo0, elo1, draw_ratio, rms_bias, elo_model) {
+    var rms_bias_score = L(rms_bias) - 0.5;
+    var variance3 = (1 - draw_ratio) / 4.0;
+    this.variance = variance3 - Math.pow(rms_bias_score, 2);
+    if (this.variance <= 0) {
+        return;
     }
-    if(elo_model=="Logistic"){
-	this.elo0=elo0;
-	this.elo1=elo1;
-	this.score0=L(elo0);
-	this.score1=L(elo1);
-    }else{  // Assume "Normalized"
-	var nt0=elo0/nelo_divided_by_nt;
-	var nt1=elo1/nelo_divided_by_nt;
-	var sigma=Math.sqrt(this.variance);
-	this.score0=nt0*sigma+0.5;
-	this.score1=nt1*sigma+0.5;
-	this.elo0=Linv(this.score0);
-	this.elo1=Linv(this.score1);
+    if (elo_model == "Logistic") {
+        this.elo0 = elo0;
+        this.elo1 = elo1;
+        this.score0 = L(elo0);
+        this.score1 = L(elo1);
+    } else {
+        // Assume "Normalized"
+        var nt0 = elo0 / nelo_divided_by_nt;
+        var nt1 = elo1 / nelo_divided_by_nt;
+        var sigma = Math.sqrt(this.variance);
+        this.score0 = nt0 * sigma + 0.5;
+        this.score1 = nt1 * sigma + 0.5;
+        this.elo0 = Linv(this.score0);
+        this.elo1 = Linv(this.score1);
     }
-    this.w2=Math.pow((this.score1-this.score0),2)/this.variance;
-    this.LA=Math.log(beta/(1-alpha));
-    this.LB=Math.log((1-beta)/alpha);
-    this.characteristics=function(elo){
-	var score=L(elo);
-	var h=(2*score-(this.score0+this.score1))/(this.score1-this.score0);
-	var PT_=PT(this.LA,this.LB,h);
-	return [PT_[0],PT_[1]/this.w2];
-    }
+    this.w2 = Math.pow(this.score1 - this.score0, 2) / this.variance;
+    this.LA = Math.log(beta / (1 - alpha));
+    this.LB = Math.log((1 - beta) / alpha);
+    this.characteristics = function (elo) {
+        var score = L(elo);
+        var h = (2 * score - (this.score0 + this.score1)) / (this.score1 - this.score0);
+        var PT_ = PT(this.LA, this.LB, h);
+        return [PT_[0], PT_[1] / this.w2];
+    };
 }
-
